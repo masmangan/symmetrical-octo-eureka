4069b8d193873781b9caad640a86080e35304e6a
Marco Mangan <marco.mangan@gmail.com>
2018-09-06 16:25:39 -0300

    #1

---
+++ src/main/java/pucrs/hello/App.java
@@ -0,0 +1,17 @@
+package pucrs.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( getSaudacao() );
+    }
+
+	static String getSaudacao() {
+		return "Olá, mundo!";
+	}
+}
